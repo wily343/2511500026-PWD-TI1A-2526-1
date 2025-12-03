dd4a8afc34fcd5b3b45ef74936064f9cd5487f84
wily343 <will.anakepa@gmail.com>
2025-12-03 10:59:18 +0700

    membuat koneksi.php

--- pertemuan-09/fungsi.php
+++ pertemuan-09/fungsi.php
@@ -1,27 +1,46 @@
 <?php
-function bersihkan ($str)
-    {
-        return htmlspecialchars(trim($str));
-    }
-        function tidakkosong($str)
-    {
-        return strlen(trim($str)) > 0;
-    }
-      
-    function formattanggal($tgl)
-    {
-        return date("d,m,y", strtotime($tgl));
+
+function bersihkan($str)
+{
+    return htmlspecialchars(trim($str), ENT_QUOTES, 'UTF-8');
+}
+
+
+function tidakkosong($str)
+{
+    return strlen(trim($str)) > 0;
+}
+
+
+function formattanggal($tgl)
+{
+    if (empty($tgl) || strtotime($tgl) === false) {
+        return $tgl; 
     }
 
-    function tampilkanBiodata($conf, $arr)
+    return date("d-m-Y", strtotime($tgl));
+}
+
+
+function tampilkanBiodata($conf, $arr)
 {
     $html = "";
+
     foreach ($conf as $k => $v) {
+
         $label  = $v["label"];
         $nilai  = bersihkan($arr[$k] ?? '');
         $suffix = $v["suffix"];
 
+    
+        if ($k === "tanggal") {
+            $nilai = formattanggal($nilai);
+        }
+
         $html .= "<p><strong>{$label}</strong> {$nilai}{$suffix}</p>";
     }
+
     return $html;
 }
+
+?>
---
+++ pertemuan-10/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_pwdd2025";
+
+$conn = mysqli_con(nect($host, $user, $pass, $db);
+
+if(!$conn) {
+    die("koneksi gagal: " . mysqli_connect_error());
+}
