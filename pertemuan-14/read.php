cef393e1f110649a2984a5ad7e62f4c12ba44c19
wily343 <will.anakepa@gmail.com>
2026-01-28 10:27:57 +0700

    membuat tabel di php my amin

--- pertemuan-14/read.php
+++ pertemuan-14/read.php
@@ -47,8 +47,8 @@
       <td><?= $i++ ?></td>
       <td>
         <a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit</a>
-        <a onclick="return confirm('HAPUS?<?= htmlspecialchars($row['cnama']); ?>?')" href="proses_delete.php?cid=<?= (int)$row['cid']; ?>">delete</a>
-    </td>
+        <a onclick="return confirm('Hapus <?= htmlspecialchars($row['cnama']); ?>?')" href="proses_delete.php?cid=<?= (int)$row['cid']; ?>">Delete</a>
+      </td>
       <td><?= $row['cid']; ?></td>
       <td><?= htmlspecialchars($row['cnama']); ?></td>
       <td><?= htmlspecialchars($row['cemail']); ?></td>
---
+++ pertemuan-16/README.md
@@ -0,0 +1,6 @@
+# pertemuan-16
+
+<ol>
+<li>menambahkan soal UAS ke file pertemuan-16<li>
+<li>menambahkan kode di php my admin<li>
+<li>membuat tabel baru nama tabel bioata dosen<li>
